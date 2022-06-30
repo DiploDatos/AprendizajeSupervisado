a397f6ba05c013e9651b6802b09cb77ca75693d5
Jose Robledo <jorobledo2@gmail.com>
2022-06-30 01:50:56 -0300

    clase_3

--- README.md
+++ README.md
@@ -11,12 +11,19 @@ Este curso requiere de la suite de paquetes que vienen por defecto en
 [Anaconda](https://www.anaconda.com/download/).  En caso de no querer instalar
 Anaconda por completo, se necesitan instalar los siguientes paquetes:
 
-- Numpy y Scipy
-- Scikit Learn
-- Matplotlib
-- Jupyter 
-- Pandas
-- Seaborn
+- [Jupyter notebook](https://jupyter-notebook.readthedocs.io/en/stable/)
+- [Numpy](https://numpy.org/)
+- [Scipy](https://scipy.org/)
+- [Matplotlib](https://matplotlib.org/)
+- [Scikit-learn](https://scikit-learn.org/stable/)
+- [Pandas](https://pandas.pydata.org/)
+- [Seaborn](https://seaborn.pydata.org/)
+- [graphviz](https://bobswift.atlassian.net/wiki/spaces/GVIZ/pages/20971549/How+to+install+Graphviz+software)
+- [Xgboost](https://xgboost.readthedocs.io/en/stable/)
+- [h5py](https://docs.h5py.org/en/stable/high/dataset.html)
+- [Surprise](http://surpriselib.com/)
+- [keras](https://keras.io/)
+- [Pytorch]()
 
 Una vez que hayan clonado este repositorio, y resuelto las dependencias,
 en este mismo directorio pueden ejecutar jupyter para abrir las notebooks:
--- Teórico/mlutils.py
+++ Teórico/mlutils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import h5py
 
 def plot_decision_boundary(model, X, y):
     # Set min and max values and give it some padding
@@ -16,3 +17,19 @@ def plot_decision_boundary(model, X, y):
     plt.ylabel('x2')
     plt.xlabel('x1')
     plt.scatter(X[0, :], X[1, :], c=y.ravel(), cmap=plt.cm.Greys)
+
+def load_dataset():
+    train_dataset = h5py.File('demo_10_dataset/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('demo_10_dataset/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
